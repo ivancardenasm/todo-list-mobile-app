324715795bf19dfe46c6dceaa4bb8b9e81dc4e8b
Ivan Cardenas Meneses <icm@athento.com>
2022-10-25 02:02:11 +0200

    fix: update status sql call

--- src/task-service.js
+++ src/task-service.js
@@ -5,17 +5,15 @@ const TaskService = class {
 
     constructor() {
         this.db = SQLite.openDatabase('database.bd');
-
         this.db.transaction(transaction => {
             transaction.executeSql(
                 `CREATE TABLE IF NOT EXISTS ${DATABASE_NAME} (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, status TEXT)`
             )
-        })
+        });
     };
 
     fetchData = (callback) => this.db.transaction(transaction => {
-        transaction.executeSql(`SELECT * FROM ${DATABASE_NAME}`,
-            null,
+        transaction.executeSql(`SELECT * FROM ${DATABASE_NAME}`, null,
             (transactionObj, {rows: {_array}}) => callback(_array),
             (transactionObj, error) => console.log('Error ', error)
         )
@@ -23,23 +21,19 @@ const TaskService = class {
 
     create = (name, status, callback) => this.db.transaction(transaction => {
         transaction.executeSql(`INSERT INTO ${DATABASE_NAME} (name, status) values (?, ?)`, [name, status],
-            (transactionObj, resultSet) => callback(transactionObj, resultSet),
+            callback,
             (transactionObj, error) => console.log('Error', error))
     });
 
-    updateStatus = (_id, newStatus, callback) => {
-        console.log('update status ', newStatus, _id)
-        this.db.transaction(transaction => {
-            transaction.executeSql(`UPDATE ${DATABASE_NAME} SET status = '?' WHERE _id = ?`, [newStatus, _id],
-                (transactionObj, resultSet) => callback(transactionObj, resultSet),
-                (transactionObj, error) => console.log('ERROR: ', error)
-                )
-        })
-    }
+    updateStatus = (_id, newStatus, callback) => this.db.transaction(transaction => {
+        transaction.executeSql(`UPDATE ${DATABASE_NAME} SET status = ? WHERE _id = ?`, [newStatus, _id],
+            callback,
+            (transactionObj, error) => console.log('ERROR: ', error))
+    });
 
     delete = (_id, callback) => this.db.transaction(transaction => {
         transaction.executeSql(`DELETE FROM ${DATABASE_NAME} WHERE _id = ? `, [_id],
-            (transactionObj, resultSet) => callback(transactionObj, resultSet))
+            callback)
     });
 };
 
